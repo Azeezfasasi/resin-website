6a59a6456dfa99edd98401f5eece2c00bf6bc790
azeezfasasi <fas.azeez@gmail.com>
2025-03-14 11:36:19 +0100

    updated mobile nav

--- src/assets/components/home-components/MobileFooter.jsx
+++ src/assets/components/home-components/MobileFooter.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import magnifier from '../../images/magnifier.svg';
 import { Link } from 'react-router-dom';
-import home from '../../images/home.png';
-import shop from '../../images/shop.png';
-import carticon from '../../images/cart.png';
 
 
 function MobileFooter() {
   return (
     <>
     <div className="h-[fit-content] fixed bottom-0 z-50 bg-white w-full flex flex-row justify-between items-center md:hidden border border-solid border-gray-700">
-        <div className="bg-[#ffffff] w-[100%] h-[fit-cntent] flex flex-row items-center justify-center gap-10 pt-[10px]">
+        <div className="bg-[#ffffff] w-[100%] h-[fit-cntent] flex flex-row items-center justify-center gap-8 pt-[10px]">
             <Link to ="/" className="w-[60px] h-[54px] flex flex-col items-center">
                 <div
-                className="text-[body-text] text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-light"
+                className="text-[body-text] text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-semibold"
                 style={{ letterSpacing: "0.1px", translate: "0 -50%" }}
                 >
                 Home
@@ -24,7 +20,7 @@ function MobileFooter() {
             </Link>
             <Link to="/app/shop" className="w-[60px] h-[54px] flex flex-col items-center">
                 <div
-                className="text-body-text text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-light"
+                className="text-body-text text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-semibold"
                 style={{ letterSpacing: "0.1px", translate: "0 -50%" }}
                 >
                 Shop
@@ -35,7 +31,7 @@ function MobileFooter() {
             </Link>
             <Link to="/app/cart" className="w-[60px] h-[54px] flex flex-col items-center">
                 <div
-                className="text-active-color-1 text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-light"
+                className="text-active-color-1 text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-semibold"
                 style={{ letterSpacing: "0.1px", translate: "0 -50%" }}
                 >
                 Cart
@@ -44,9 +40,9 @@ function MobileFooter() {
                     <i className='fa fa-shopping-cart text-[24px]'></i>
                 </div>
             </Link>
-            <Link to="/app/myaccount" className="w-[60px] h-[54px] flex flex-col items-center border">
+            <Link to="/app/myaccount" className="w-[60px] h-[54px] flex flex-col items-center">
                 <div
-                className="text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-light"
+                className="text-center font-['YuGothicUi-Light',_sans-serif] text-[13px] font-semibold"
                 style={{ letterSpacing: "0.1px", translate: "0 -50%" }}
                 >
                 Account
